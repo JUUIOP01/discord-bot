50a2ba6e596cdc842f89a75735b1d90f91dde2de
JUUIOP01 <wsadamoss@gmail.com>
2026-03-01 23:55:26 +0100

    Create register.js

---
+++ register.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const APP_ID = "TON_APPLICATION_ID";
+const TOKEN = process.env.TOKEN;
+
+const command = {
+  name: "buy",
+  description: "Acheter un produit",
+};
+
+await fetch(`https://discord.com/api/v10/applications/${APP_ID}/commands`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bot ${TOKEN}`,
+  },
+  body: JSON.stringify(command),
+});
+
+console.log("Commande enregistrée !");
